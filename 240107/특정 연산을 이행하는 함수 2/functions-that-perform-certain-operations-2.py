f5ed1bbf08fd1b8d2db1dfb0525192208402fb6f
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-07 00:58:09 +0900

    Title: 특정 연산을 이행하는 함수 2, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

--- 240107/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.py
+++ 240107/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.py
@@ -3,6 +3,9 @@ from math import ceil, floor
 a = list(map(float, input().split()))
 maxVal = max(a)
 minVal = min(a)
+for i in range(3):
+    if a[i] not in [minVal, maxVal]:
+        mid = a[i]
 
 
-print(f'{ceil(maxVal)} {floor(minVal)} {round(a[2])}')
+print(f'{ceil(maxVal)} {floor(minVal)} {round(mid)}')
