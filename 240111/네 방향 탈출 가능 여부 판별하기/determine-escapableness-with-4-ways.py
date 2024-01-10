2492dfd4e8160db4624a34f902718e2808df4454
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-11 05:36:53 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240111/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,34 @@
+from collections import deque
+# 입력
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+# bfs
+def bfs(start):
+    q = deque([start])
+    while q:
+        v = q.popleft()
+
+# dfs
+visited = [[False]*N for _ in range(N)]
+escape = 0
+def dfs(i,j):
+    global escape
+    dis, djs = [0,0,-1,1],[-1,1,0,0]
+
+    for di,dj in zip(dis, djs):
+        i2,j2 = i+di, j+dj
+
+        if 0<=i2<N and 0<=j2<N and not visited[i2][j2] and grid[i2][j2]:
+            visited[i2][j2] = True
+            if (i2, j2) == (N-1, M-1):
+                escape = 1
+                return
+            dfs(i2,j2)
+
+
+
+# 본문
+dfs(0,0)
+visited[0][0] = True
+print(escape)
