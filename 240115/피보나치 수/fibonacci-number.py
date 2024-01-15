99082cca595dcc206a9345a1b19cc25b7e471e5a
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-15 23:11:37 +0900

    Title: 피보나치 수, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240115/피보나치 수/fibonacci-number.py
+++ 240115/피보나치 수/fibonacci-number.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 def fibo(n):
-    if memo[n] != 0:
-        return memo[n]
-    return fibo(n-1) + fibo(n-2)
+    if memo[n] == 0:
+        memo[n] = fibo(n-1) + fibo(n-2)
+    return memo[n]
 
 memo = [0]*51
 memo[1] = 1
