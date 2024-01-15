c36d779f2d41604e542ccbc97267824e490643eb
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-16 00:25:51 +0900

    Title: 아름다운 수, Time: 152ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240116/아름다운 수/beautiful-number.py
+++ 240116/아름다운 수/beautiful-number.py
@@ -3,10 +3,9 @@ memo = [0]*11
 
 memo[1]=1
 memo[2]=2
-memo[3]=memo[3-1]*memo[1] + memo[3-2]*memo[2]
+memo[3]=memo[3-1] + memo[3-2] + 1
 
 for i in range(4,10):
-    for j in range(1, i):
-        memo[i] += memo[i-j]*memo[j]
+    memo[i] = sum(memo[1:i])+1
 
 print(memo[n])
