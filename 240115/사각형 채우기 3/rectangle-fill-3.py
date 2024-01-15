7cca1a7e1215a446d665ee0116b1c67b78e67998
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-15 23:36:39 +0900

    Title: 사각형 채우기 3, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/사각형 채우기 3/rectangle-fill-3.py
+++ 240115/사각형 채우기 3/rectangle-fill-3.py
@@ -4,6 +4,6 @@ memo[0]=1
 memo[1]=2
 memo[2]=7
 for i in range(3,n+1):
-    memo[i] = (2*memo[i-1]+3*memo[i-2]+2*memo[i-3])%1_000_000_007
+    memo[i] = (2*memo[i-1]+3*memo[i-2]+2*memo[i-3])%1000000007
 
 print(memo[n])
