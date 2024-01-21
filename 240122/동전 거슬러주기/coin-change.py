f751979c6b39461aafc641d435e7db9361bbb4e0
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-22 03:50:05 +0900

    Title: 동전 거슬러주기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/동전 거슬러주기/coin-change.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+coins = list(map(int, input().split()))
+
+INF = 1_000_000_000
+dp = [INF]*(M+1)
+dp[0] = 0
+
+for i in range(1, M+1):
+    for c in coins:
+        if i-c >=0:
+            dp[i] = min(dp[i], dp[i-c]+1)
+
+print(dp[M])
