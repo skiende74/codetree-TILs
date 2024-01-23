f19c0dc760ba70dc84ed297ee208c3776eb2474d
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-24 05:20:50 +0900

    Title: 부분 수열의 합, Time: 5092ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 240124/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+seq = list(map(int, input().split()))
+
+seq.sort()
+
+answer = False
+total = 0
+def dfs(num, i):
+    global answer
+    if not i<N:
+        return
+    if num == seq[i]:
+        answer = True
+        return
+       
+    dfs(num-seq[i], i+1)
+    dfs(num, i+1)
+
+dfs(M, 0)
+
+print('Yes' if answer else 'No')
