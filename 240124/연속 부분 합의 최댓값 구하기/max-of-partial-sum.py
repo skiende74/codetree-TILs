7b728860978df3b5abedb9abb7f2dfa385e0db27
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-24 07:46:29 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240124/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -0,0 +1,16 @@
+import sys
+
+# 입력
+N = int(input())
+seq = list(map(int, input().split()))
+
+# 초기화
+INT_MIN = -sys.maxsize
+dp = [INT_MIN]*N
+
+dp[0] = 0
+# 본문
+for i in range(1, N):
+    dp[i] = max(dp[i-1]+seq[i], seq[i])
+
+print(max(dp[1:]))
