8b4bc3e450ae158a9efc7f9b0ef30ef20859304d
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-26 22:03:15 +0900

    Title: 올바른 등식 만들기, Time: 221ms, Memory: 25MB, Status: Passed - CodeTree

--- 240126/올바른 등식 만들기/right-equality.py
+++ 240126/올바른 등식 만들기/right-equality.py
@@ -6,8 +6,8 @@ dp = [[0]*20+[1]+[0]*20]+[ [0]*41 for _ in range(N)]
 for i in range(1, N+1):
     num = seq[i]
     for j in range(-20, 20+1):
-        if -20<=j-num<20:
+        if -20<=j-num<=20:
             dp[i][j+20] += dp[i-1][j-num+20]
-        if -20<=j+num<20:
+        if -20<=j+num<=20:
             dp[i][j+20] += dp[i-1][j+num+20]
 print(dp[N][M+20])
