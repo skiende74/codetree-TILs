e87b6b3c2349fb012df355b60475ff91f8eb3a5a
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-27 06:02:20 +0900

    Title: 1차원 폭발 게임, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240127/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,37 @@
+N, M = map(int, input().split())
+seq = [int(input()) for _ in range(N)]
+
+while True:
+    old = seq[0]
+    count = 1
+    for i in range(N):
+        if old == seq[i]:
+            count += 1
+        elif count>=M:
+            for j in range(1, count+1):
+                seq[i-j] = 0
+        old = seq[i]
+
+    new_seq = [-1]*N
+    new_idx = 0
+    is_zero_occur = False
+    for i in range(N):
+        if seq[i] != 0:
+            new_seq[new_idx] = seq[i]
+            new_idx += 1
+        else:
+            is_zero_occur = True
+    seq = new_seq    
+    if not is_zero_occur:
+        break
+
+for i in range(N):
+    if seq[i] == -1:
+        print(i)
+        break
+    print(seq[i])
+
+for i in range(N):
+    if seq[i] == -1:
+        break
+    print(seq[i])
