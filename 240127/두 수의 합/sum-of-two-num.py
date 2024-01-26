669fef56a36f897c747ec17a6651b5fd98a9ea5d
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-27 03:26:42 +0900

    Title: 두 수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240127/두 수의 합/sum-of-two-num.py
+++ 240127/두 수의 합/sum-of-two-num.py
@@ -1,11 +1,11 @@
+from collections import Counter
 from itertools import combinations
-
 N, K = map(int, input().split())
 seq = list(map(int, input().split()))
 
 result = 0
-for a,b in combinations(seq, 2):
+for ((a,a_c),(b,b_c)) in combinations(Counter(seq).items(), 2):
     if a+b == K:
-        result +=1
+        result +=a+c*b_c
 
 print(result)
