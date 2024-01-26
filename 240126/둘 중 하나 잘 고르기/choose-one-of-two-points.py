278680e8fc999d4296ec9f92c9d8d8289f56a1e9
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-26 21:33:31 +0900

    Title: 둘 중 하나 잘 고르기, Time: 129ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240126/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 240126/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -5,9 +5,10 @@ dp = [[0]*(N+1) for _ in range(2*N+1)]
 
 for i in range(1, 2*N+1):
     a,b = cards[i]
-    for j in range(N+1):
+    for j in range(min(i,N)+1):
         if j >=1:
             dp[i][j] = max(dp[i][j], dp[i-1][j-1] + a)
         dp[i][j] = max(dp[i][j], dp[i-1][j]+b)
 
+#print(dp)
 print(dp[2*N][N])
