43a062f7a3bede7235a8ea4bceba4921f6050388
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-27 06:20:24 +0900

    Title: 1차원 폭발 게임, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240127/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240127/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,41 +1,29 @@
 N, M = map(int, input().split())
-seq = [int(input()) for _ in range(N)]
+seq = list(reversed([int(input()) for _ in range(N)]))
 
 while True:
-    old = seq[0]
     count = 1
-    for i in range(N):
-        if old == seq[i]:
+    for i in range(1, len(seq)):
+        if seq[i] == seq[i-1]:
             count += 1
         elif count>=M:
             for j in range(1, count+1):
                 seq[i-j] = 0
             count = 1
-        old = seq[i]
+    
     if count>=M:
-        for j in range(1, count+1):
-            seq[N-1-j] = 0
+        seq = seq[:-count]
 
-    new_seq = [-1]*N
-    new_idx = 0
-    is_zero_occur = False
-    for i in range(N):
+    if 0 not in seq:
+        break
+    
+    new_seq = []
+    for i in range(len(seq)):
         if seq[i] != 0:
-            new_seq[new_idx] = seq[i]
-            new_idx += 1
-        else:
-            is_zero_occur = True
+            new_seq.append(seq[i])
     seq = new_seq    
-    if not is_zero_occur:
-        break
+    
+print(len(seq))
 
-for i in range(N):
-    if seq[i] == -1:
-        print(i)
-        break
-    print(seq[i])
-
-for i in range(N):
-    if seq[i] == -1:
-        break
-    print(seq[i])
+for i in range(len(seq)):
+    print(seq[-1-i])
