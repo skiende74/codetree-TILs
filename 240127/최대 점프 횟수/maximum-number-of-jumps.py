402e70c411bef09b6c8e88349d6fb3f8abcb3ac0
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-27 15:22:13 +0900

    Title: 최대 점프 횟수, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240127/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240127/최대 점프 횟수/maximum-number-of-jumps.py
@@ -7,7 +7,7 @@ dp = [0] +[MIN]*(N-1)
 
 for i in range(1, N):
     for j in range(i):
-        if seq[j]>=i-j and dp[i] < dp[j]+1:
+        if seq[j]>=i-j and dp[i] < dp[j]+1 and dp[j] != MIN:
             dp[i] = dp[j]+1
 
 print(max(dp))
