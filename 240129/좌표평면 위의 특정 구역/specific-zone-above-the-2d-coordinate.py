5714629715fccfb62d2a31e561728e3a8de60202
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-29 03:51:39 +0900

    Title: 좌표평면 위의 특정 구역, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240129/좌표평면 위의 특정 구역/specific-zone-above-the-2d-coordinate.py
+++ 240129/좌표평면 위의 특정 구역/specific-zone-above-the-2d-coordinate.py
@@ -7,16 +7,16 @@ y0 = (0,points[0][1])
 x1,y1 = x0,y0
 for i,(x,y) in enumerate(points):
     
-    if x<x0[1]:
+    if x<=x0[1]:
         x0_2 = x0
         x0 = (i, x)
-    if x>x1[1]:
+    if x>=x1[1]:
         x1_2 = x1
         x1 = (i, x)
-    if y<y0[1]:
+    if y<=y0[1]:
         y0_2 = y0
         y0 = (i, y)
-    if y>y1[1]:
+    if y>=y1[1]:
         y1_2 = y1
         y1 = (i, y)
 
