9c67b00d4fb423dad822d07e65198c9e5451637f
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-29 02:39:25 +0900

    Title: 대폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240129/대폭발/big-explosion.py
+++ 240129/대폭발/big-explosion.py
@@ -3,15 +3,15 @@ from collections import defaultdict
 N,M,r,c = map(int, input().split())
 
 count = defaultdict(lambda:0)
-visited = [[[False]*N for _ in range(N)] for _ in range(M+1)]
+visited = [[[False]*N for _ in range(N)]]
 def dfs(i,j, t):
     if t==M:
         return
 
-    dis, djs = [-1,1,0,0],[0,0,-1,1]
+    dis, djs = [-1,1,0,0,0],[0,0,-1,1,0]
     for di, dj in zip(dis,djs):
         i2,j2 = i+(2**t)*di, j+(2**t)*dj
-        if 0<=i2<N and 0<=j2<N and not visited[t+1][i2][j2]:
+        if 0<=i2<N and 0<=j2<N and not visited[i2][j2]:
             visited[t+1][i2][j2] = True
             count[t+1] += 1
             dfs(i2,j2, t+1)
