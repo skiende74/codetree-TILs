5edb7641733d8917ab315392ce9b81cdb8031201
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-29 04:10:52 +0900

    Title: 좌표평면 위의 특정 구역, Time: 265ms, Memory: 27MB, Status: Passed - CodeTree

--- 240129/좌표평면 위의 특정 구역/specific-zone-above-the-2d-coordinate.py
+++ 240129/좌표평면 위의 특정 구역/specific-zone-above-the-2d-coordinate.py
@@ -1,45 +1,51 @@
+from copy import copy
+
 N = int(input())
 points = [list(map(int,input().split())) for _ in range(N)]
 
 result = 100000000000
-x0 = (0,points[0][0])
-y0 = (0,points[0][1])
-x1,y1 = x0,y0
-for i,(x,y) in enumerate(points):
+
+
+def min2(x, key, sel):
+    x = copy(x)
     
-    if x<=x0[1]:
-        x0_2 = x0
-        x0 = (i, x)
-    if x>=x1[1]:
-        x1_2 = x1
-        x1 = (i, x)
-    if y<=y0[1]:
-        y0_2 = y0
-        y0 = (i, y)
-    if y>=y1[1]:
-        y1_2 = y1
-        y1 = (i, y)
+    min_x = min(x, key=key)
+    min_idx = x.index(min_x)
+    x.pop(min_idx)
+    min_x2 = min(x, key=key)
+    min_idx2 = x.index(min_x2)
+    if min_idx<=min_idx2:
+        min_idx2 += 1
+    
+    return [[min_x[sel],min_idx],[min_x2[sel], min_idx2]]
+[[x0, x0_idx],[x0_2,_]] = min2(points, lambda x:x[0], 0)
+[[x1, x1_idx],[x1_2,_]] = min2(points, lambda x:-x[0], 0)
+[[y0, y0_idx],[y0_2,_]] = min2(points, lambda x:x[1], 1)
+[[y1, y1_idx],[y1_2,_]] = min2(points, lambda x:-x[1], 1)
 
 for i in range(N):
-    if i != x0[0]:
-        xx0=x0[1]
+    if i != x0_idx:
+        xx0 = x0
     else:
-        xx0=x0_2[1]
+        xx0=x0_2
 
-    if i != x1[0]:
-        xx1=x1[1]
+    if i != x1_idx:
+        xx1=x1
     else:
-        xx1=x1_2[1]
+        xx1=x1_2
 
-    if i != y0[0]:
-        yy0=y0[1]
+    if i != y0_idx:
+        yy0=y0
     else:
-        yy0=y0_2[1]
+        yy0=y0_2
 
-    if i != y1[0]:
-        yy1=y1[1]
+    if i != y1_idx:
+        yy1=y1
     else:
-        yy1=y1_2[1]
+        yy1=y1_2
 
     result = min(result, (xx1-xx0)*(yy1-yy0))
+# print(x0,x0_2,y0,y0_2)
+# print(x1,x1_2,y1,y1_2)
+# print(x0_idx, y0_idx, x1_idx, y1_idx)
 print(result)
