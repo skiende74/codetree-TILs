948f835218b77f9ee68ed54fb63c6208459e0889
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-29 05:09:59 +0900

    Title: 요일 맞추기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/요일 맞추기/guess-day-of-week.py
+++ 240129/요일 맞추기/guess-day-of-week.py
@@ -3,17 +3,11 @@ m1,d1, m2,d2 = map(int,input().split())
 num_of_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 def get_elapsed_day(m1,d1):
-    m,d = 1,1
     elapsed_day = 0
-    while True:
-        if (m,d) == (m1,d1):
-            return elapsed_day
-        d += 1
-
-        if d > num_of_days[m]:
-            d=1
-            m+=1
-        elapsed_day +=1
+    for i in range(1, m1):
+        elapsed_day += num_of_days[i]
+    elapsed_day += d1
+    return elapsed_day
 
 diff = get_elapsed_day(m2,d2) - get_elapsed_day(m1,d1)
 day_of_week = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
