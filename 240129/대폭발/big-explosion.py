81237ad00c8357d00e54198c32984cba5868c629
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-29 02:45:06 +0900

    Title: 대폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240129/대폭발/big-explosion.py
+++ 240129/대폭발/big-explosion.py
@@ -1,21 +1,24 @@
 from collections import defaultdict
+from copy import deepcopy
 
 N,M,r,c = map(int, input().split())
-
-count = defaultdict(lambda:0)
-visited = [[[False]*N for _ in range(N)]]
-def dfs(i,j, t):
-    if t==M:
-        return
-
-    dis, djs = [-1,1,0,0,0],[0,0,-1,1,0]
-    for di, dj in zip(dis,djs):
-        i2,j2 = i+(2**t)*di, j+(2**t)*dj
-        if 0<=i2<N and 0<=j2<N and not visited[i2][j2]:
-            visited[t+1][i2][j2] = True
-            count[t+1] += 1
-            dfs(i2,j2, t+1)
+r,c = r-1, c-1
+grid = [[False]*N for _ in range(N)]
+grid[r][c] = True
+for t in range(M):
+    new_grid = deepcopy(grid)
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j]:
+                dis,djs = [-1,1,0,0],[0,0,-1,1]
+                for di,dj in zip(dis,djs):
+                    k = 2**t
+                    i2,j2= i+k*di, j+k*dj
+                    new_grid[i2][j2] = True
     
-r,c = r-1,c-1
-dfs(r,c,0)
-print(count[m])
+    grid = new_grid
+
+total = 0
+for i in range(N):
+    total += sum(grid[i])
+print(total)
