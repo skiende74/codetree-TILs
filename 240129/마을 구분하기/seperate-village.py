ee005c27849d7ff1d880ace419acebd5b633393d
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-29 15:24:22 +0900

    Title: 마을 구분하기, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240129/마을 구분하기/seperate-village.py
+++ 240129/마을 구분하기/seperate-village.py
@@ -1,8 +1,9 @@
 # 입력
 N = int(input())
-grid = [ list(map(int, input().split())) for _ in range(N)]
+grid = [list(map(int, input().split())) for _ in range(N)]
 
 # dfs
+visited = [[False for _ in range(N)] for _ in range(N)]
 def dfs(i, j):
     global in_count
     dis, djs = [0,0,-1,1], [-1,1,0,0]
@@ -11,19 +12,19 @@ def dfs(i, j):
         if 0<=i2<N and 0<=j2<N and not visited[i2][j2] and grid[i2][j2] == 1:
             visited[i2][j2] = True
             in_count += 1
-            dfs(i2,j2)
+            dfs(i2, j2)
 
-visited = [ [False]*N for _ in range(N)]
+# 본문
 result = []
 for i in range(N):
     for j in range(N):
-        in_count = 1
-        if not visited[i][j] and grid[i][j] == 1:
-            visited[i][j]=True
-            dfs(i,j)
+        if not visited[i][j] and grid[i][j]==1:
+            in_count = 1
+            visited[i][j] = True
+            dfs(i, j)
             result.append(in_count)
 
 result.sort()
 print(len(result))
-for w in result:
-    print(w)
+for r in result:
+    print(r)
