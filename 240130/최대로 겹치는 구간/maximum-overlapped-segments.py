241169c79540a31bc098dddada504dba580ef322
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-30 02:08:59 +0900

    Title: 최대로 겹치는 구간, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240130/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240130/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,9 +1,12 @@
+OFFSET = 100
+MAX_R = 200
+
 N = int(input())
 segments = [list(map(int, input().split())) for _ in range(N)]
 
-seq = [0]*(201)
+seq = [0]*(MAX_R+1)
 for s,e in segments:
     for i in range(s,e):
-        seq[i+100] += 1
+        seq[i+OFFSET] += 1
 
 print(max(seq))
