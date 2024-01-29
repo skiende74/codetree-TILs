cedcf57b24c411f33953cfccb08651054bce9eaf
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-29 15:17:39 +0900

    Title: 마을 구분하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240129/마을 구분하기/seperate-village.py
@@ -0,0 +1,29 @@
+# 입력
+N = int(input())
+grid = [ list(map(int, input().split())) for _ in range(N)]
+
+# dfs
+def dfs(i, j):
+    global in_count
+    dis, djs = [0,0,-1,1], [-1,1,0,0]
+    for di, dj in zip(dis, djs):
+        i2, j2 = i+di, j+dj
+        if 0<=i2<N and 0<=j2<N and not visited[i2][j2] and grid[i2][j2] == 1:
+            visited[i2][j2] = True
+            in_count += 1
+            dfs(i2,j2)
+
+visited = [ [False]*N for _ in range(N)]
+result = []
+for i in range(N):
+    for j in range(N):
+        in_count = 1
+        if not visited[i][j] and grid[i][j] == 1:
+            visited[i][j]=True
+            dfs(i,j)
+            result.append(in_count)
+
+result.sort()
+print(len(in_count))
+for w in result:
+    print(w)
