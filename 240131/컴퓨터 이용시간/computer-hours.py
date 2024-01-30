ff10149ddb563239b857e1d3541be0e0db49ad9a
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-01-31 01:12:26 +0900

    Title: 컴퓨터 이용시간, Time: 1072ms, Memory: 51MB, Status: Passed - CodeTree

---
+++ 240131/컴퓨터 이용시간/computer-hours.py
@@ -0,0 +1,25 @@
+from heapq import heappush, heappop
+
+N = int(input())
+segments = [list(map(int,input().split())) for _ in range(N)]
+computers = list(range(1,N+1))
+
+points= []
+for index,(s,e) in enumerate(segments):
+    points.append((s,1,index))
+    points.append((e,-1,index))
+points.sort()
+
+ans = [0]*(N)
+
+
+
+for x,v,person_idx in points:
+    if v==1:
+        computer_idx = heappop(computers)
+        ans[person_idx] = computer_idx
+    else:
+        heappush(computers, ans[person_idx])
+
+
+print(' '.join(map(str, ans)))
