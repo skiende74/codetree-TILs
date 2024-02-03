0fd4b942a9117b49448d2fd365f03ce374868661
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 02:02:22 +0900

    Title: 편집거리 3, Time: 153ms, Memory: 31MB, Status: Passed - CodeTree

--- 240204/편집거리 3/edit-distance-3.py
+++ 240204/편집거리 3/edit-distance-3.py
@@ -12,6 +12,6 @@ for i in range(1, M+1):
         if A[i-1]==B[j-1]:
             dp[i][j] = dp[i-1][j-1]
         else:
-            dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]) + 1
+            dp[i][j] = min(dp[i-1][j-1]+1,dp[i-1][j],dp[i][j-1]) + 1
 
 print(dp[-1][-1])
