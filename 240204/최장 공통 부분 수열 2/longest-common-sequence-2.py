03cb62997f45445d0f3b25802e773de6d3e520b5
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 06:59:01 +0900

    Title: 최장 공통 부분 수열 2, Time: 148ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240204/최장 공통 부분 수열 2/longest-common-sequence-2.py
+++ 240204/최장 공통 부분 수열 2/longest-common-sequence-2.py
@@ -6,7 +6,7 @@ dp = [['' for _ in range(N+1)] for _ in range(M+1)]
 for i in range(1, M+1):
     for j in range(1, N+1):
         if A[i-1] == B[j-1]:
-            dp[i][j] = dp[i-1][j-1] + A[i]
+            dp[i][j] = dp[i-1][j-1] + A[i-1]
         elif dp[i][j-1]>dp[i-1][j]:
             dp[i][j] = dp[i][j-1]
         else:
