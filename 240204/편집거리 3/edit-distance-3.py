979f6394913de079e1f6e3b86e49ab6a32e260e5
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 02:01:08 +0900

    Title: 편집거리 3, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240204/편집거리 3/edit-distance-3.py
@@ -0,0 +1,17 @@
+A,B = input(), input()
+M,N = len(A), len(B)
+
+dp = [[0]*(N+1) for _ in range(M+1)]
+for i in range(1,M+1):
+    dp[i][0] = i
+for j in range(1, N+1):
+    dp[0][j] = j
+
+for i in range(1, M+1):
+    for j in range(1, N+1):
+        if A[i-1]==B[j-1]:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]) + 1
+
+print(dp[-1][-1])
