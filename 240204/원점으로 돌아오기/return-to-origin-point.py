cbc6044a603e28b2ea06bccdbfae5b994590b6f7
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 07:34:28 +0900

    Title: 원점으로 돌아오기, Time: 672ms, Memory: 28MB, Status: Passed - CodeTree

--- 240204/원점으로 돌아오기/return-to-origin-point.py
+++ 240204/원점으로 돌아오기/return-to-origin-point.py
@@ -10,22 +10,35 @@ for i in range(N+1):
                 graph[i].append(j)
                 graph[j].append(i)
 
-def dfs(i):
+def dfs(i, direction):
     global answer
     if len(result) == N+2 and result[-1] == start:
         answer += 1
         return
     for j in graph[i]:
-        if not visited[j]:
+        if not visited[j] and not is_same_dir(i,j,direction):
             visited[j] = True
             result.append(j)
-            dfs(j)
+            dfs(j, get_direction(points[i], points[j]))
             result.pop()
             visited[j] = False
 
+RIGHT,LEFT, UP, DOWN = 0,1,2,3
+def get_direction(point1, point2):
+    if point2[0] > point1[0]:
+        return RIGHT
+    if point2[0] < point1[0]:
+        return LEFT
+    if point2[1] > point1[1]:
+        return UP
+    return DOWN
+
+def is_same_dir(i, j, direction):
+    direction2 = get_direction(points[i], points[j])
+    return direction == direction2
 answer = 0
 start = 0
 result = [0]
 visited = [False]*(N+1)
-dfs(start)
+dfs(start, None)
 print(answer)
