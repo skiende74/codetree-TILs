ba83021c6634d9298ca7dd7b942a0475ff843d04
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 22:06:51 +0900

    Title: 공통 부분 문자열, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240204/공통 부분 문자열/common-substring.py
+++ 240204/공통 부분 문자열/common-substring.py
@@ -7,6 +7,7 @@ for i in range(1,M+1):
     for j in range(1,N+1):
         if A[i-1]==B[j-1]:
             dp[i][j] = dp[i-1][j-1]+1
-        dp[i][j] = max(dp[i][j], dp[i-1][j], dp[i-1][j-1])
+        else:
+            dp[i][j] = max(dp[i][j], dp[i-1][j], dp[i-1][j-1])
 
 print(dp[-1][-1])
