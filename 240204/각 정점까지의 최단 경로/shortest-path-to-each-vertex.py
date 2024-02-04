44c2bb93f24907b59d837577d1f73d5b8f1c28b8
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 15:08:57 +0900

    Title: 각 정점까지의 최단 경로, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240204/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -0,0 +1,27 @@
+from heapq import heappush, heappop
+
+readlist = lambda:list(map(int,input().split()))
+N, M = readlist()
+k = int(input())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    i,j,d = readlist()
+    graph[i].append((j, d))
+    graph[j].append((i, d))
+
+import sys
+MAX=sys.maxsize
+dist = [MAX]*(N+1)
+def dijkstra(start):
+    pq = [(0,start)]
+    dist[start] = 0
+    while pq:
+        d, i = heappop(pq)
+
+        for i2, d2 in graph[i]:
+            if d + d2 < dist[i2]:
+                dist[i2] = d+d2
+                heappush(pq, (dist[i2], i2))
+
+dijkstra(k)
+print('\n'.join(map(str, dist[1:])))
