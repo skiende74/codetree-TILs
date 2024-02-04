1f7b10c6ce7eb5ca9588f0880e79f972233d3407
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-05 08:44:42 +0900

    Title: 정수 n개의 합 2, Time: 147ms, Memory: 31MB, Status: Passed - CodeTree

--- 240205/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 240205/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -1,11 +1,14 @@
 N, K = map(int, input().split())
 seq = list(map(int, input().split()))
 
+prefix_sum = [0]*(N+1)
 total = sum(seq[:K])
 max_total = total
 
+for i in range(N):
+    prefix_sum[i] = prefix_sum[i-1] + seq[i-1]
+
 for i in range(K, N):
-    total += seq[i] - seq[i-K]
-    max_total = max(max_total, total)
+    max_total = max(max_total, prefix_sum[i] - prefix_sum[i-K])
 
 print(max_total)
