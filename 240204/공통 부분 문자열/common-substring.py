b05820a2414bb67ff87e6857cd848da6754d3f37
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 22:23:06 +0900

    Title: 공통 부분 문자열, Time: 1342ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240204/공통 부분 문자열/common-substring.py
+++ 240204/공통 부분 문자열/common-substring.py
@@ -8,6 +8,6 @@ for i in range(1,M+1):
             dp[i][j] = [dp[i-1][j-1][0]+1, dp[i-1][j-1][0]+1]
         else:
             dp[i][j][0] = 0
-        dp[i][j][1] = max(dp[i-1][j][1], dp[i][j-1][1], dp[i-1][j-1][1])
+        dp[i][j][1] = max(dp[i-1][j][1], dp[i][j-1][1], dp[i-1][j-1][1],dp[i][j][1])
 
 print(dp[-1][-1][1])
