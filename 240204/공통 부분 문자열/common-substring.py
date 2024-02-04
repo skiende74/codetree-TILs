53839147d1ae4701c69e4f9605b508a51cbf505f
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 22:02:17 +0900

    Title: 공통 부분 문자열, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240204/공통 부분 문자열/common-substring.py
+++ 240204/공통 부분 문자열/common-substring.py
@@ -3,8 +3,8 @@ M, N = len(A), len(B)
 
 dp=[[0]*(N+1) for _ in range(M+1)]
 
-for i in range(M):
-    for j in range(N):
+for i in range(1,M+1):
+    for j in range(1,N+1):
         if A[i-1]==B[j-1]:
             dp[i][j] = dp[i-1][j-1]+1
         else:
