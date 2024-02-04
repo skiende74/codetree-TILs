b621916461a88799fd49439c6247dfaf417e65dd
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 14:36:31 +0900

    Title: 숫자의 개수, Time: 659ms, Memory: 33MB, Status: Passed - CodeTree

--- 240204/숫자의 개수/number-of-integers.py
+++ 240204/숫자의 개수/number-of-integers.py
@@ -1,7 +1,33 @@
-from collections import Counter
 N, M = map(int, input().split())
 seq = list(map(int, input().split()))
-c = Counter(seq)
+
+def binary_lower(target):
+    left, right = 0, N-1
+    min_idx = N
+    
+    while left <= right:
+        mid = (left + right)//2
+        if seq[mid] >= target:
+            min_idx = min(min_idx, mid)
+            right = mid-1
+        else:
+            left = mid + 1
+    
+    return min_idx
+def binary_custom(target):
+    left, right = 0, N-1
+    max_idx = -1
+    
+    while left <= right:
+        mid = (left + right)//2
+        if seq[mid] <= target:
+            max_idx = max(max_idx, mid)
+            left = mid+1
+        else:
+            right = mid - 1
+    
+    return max_idx
 
 for _ in range(M):
-    print(c[int(input())])
+    target = int(input())
+    print(binary_custom(target)-binary_lower(target)+1)
