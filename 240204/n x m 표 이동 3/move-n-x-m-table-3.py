01c28db3925b1f8fa063a841c3d194a733550871
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-04 22:39:34 +0900

    Title: n x m 표 이동 3, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240204/n x m 표 이동 3/move-n-x-m-table-3.py
+++ 240204/n x m 표 이동 3/move-n-x-m-table-3.py
@@ -4,10 +4,11 @@ obstacles = [list(map(int,input().split())) for _ in range(K)]
 blocked = [[[False,False] for _ in range(M+2)] for _ in range(N+2)]
 
 for a,b,c,d in obstacles:
+    [(a,b),(c,d)]=sorted([(a,b),(c,d)])
     dr = (c-a,d-b)
-    if dr == (1,0):
-        blocked[c+1][d+1][0] = True
     if dr == (0,1):
+        blocked[c+1][d+1][0] = True
+    if dr == (1,0):
         blocked[c+1][d+1][1] = True
     
 
@@ -17,7 +18,7 @@ dp[1][1] = 1
 for i in range(1, N+2):
     for j in range(1, M+2):
         if not blocked[i][j][0]:
-            dp[i][j] = dp[i][j-1]
+            dp[i][j] += dp[i][j-1]
         if not blocked[i][j][1]:
             dp[i][j] += dp[i-1][j]
 
