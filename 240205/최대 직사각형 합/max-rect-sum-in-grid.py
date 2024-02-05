faf96a65d3681b48bb91f565cd489f7f50b91e9b
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-05 11:57:40 +0900

    Title: 최대 직사각형 합, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240205/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -0,0 +1,22 @@
+N = int(input())
+grid = [[0]*(N+1)] + [[0] + list(map(int,input().split())) for _ in range(N) ]
+
+prefix_sum = [[0]*(N+1) for _ in range(N+1)]
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        prefix_sum[i][j] = -prefix_sum[i-1][j-1] + prefix_sum[i][j-1] + \
+        prefix_sum[i-1][j] + grid[i][j]
+
+ans = -1000_000_000
+dp = [0]*(N+1)
+for i1 in range(1,N+1):
+    for i2 in range(i1, N+1):
+        dp = [0]*(N+1)
+        for j in range(1, N+1):
+            now = prefix_sum[i2][j] -\
+            prefix_sum[i2][j-1] - prefix_sum[i1-1][j] + prefix_sum[i1-1][j-1]
+
+            dp[j] = max(dp[j-1]+now, now)
+        ans = max(ans, dp[-1])
+
+print(ans)
