9f03e635d4c3549249c254c6b299e3dfc6f53c3a
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-10 06:20:48 +0900

    Title: 알파벳과 사칙연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240210/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,40 @@
+import sys
+sys.setrecursionlimit(100000)
+def get_num(char):
+    return selected_nums[ord(char) - ord('a')]
+def dfs():
+    global ans
+    if len(selected_nums) == N+1:
+        ans = max(ans, calc())
+        return
+
+    for j in range(1,5):
+        selected_nums.append(j)
+        dfs()
+        selected_nums.pop()
+
+def calc():
+    result = get_num(alphas[0])
+    for alpha, op in zip(alphas[1:], ops):
+        if op == '+':
+            result *= get_num(alpha)
+        if op == '-':
+            result -= get_num(alpha)
+        if op == '*':
+            result *= get_num(alpha)
+        if op == '/':
+            result /= get_num(alpha)
+    return result
+
+eq = input()
+selected_nums = []
+alphas, ops = [eq[0]] , []
+for i in range(1, len(eq)):
+    if i%2 == 1:
+        ops.append(eq[i])
+    else:
+        alphas.append(eq[i])
+N = len(ops)
+ans = -2**31
+dfs()
+print(ans)
