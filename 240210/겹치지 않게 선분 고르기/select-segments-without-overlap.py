d2076e659de7e00d2269f56dfcb19a0d5f221666
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-10 04:21:24 +0900

    Title: 겹치지 않게 선분 고르기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240210/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240210/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -1,18 +1,31 @@
 N = int(input())
 segments = [list(map(int,input().split())) for _ in range(N)]
+segments_selected = []
 
-points = []
-for s, e in segments:
-    points.append((s,+1))
-    points.append((e,-1))
-
-points.sort(key=lambda x: (x[0],-x[1]))
-
-count = 0
 ans = 0
-for x,v in points:
-    if count == 0 and v==1:
-        ans += 1
-    count += v
+def dfs(i):
+    global ans
+    if i==N and is_possible():
+        ans = max(ans, len(segments_selected))
+        return
 
+    dfs(i+1)
+    segments_selected.append(segments[i])
+    dfs(i+1)
+    segments_selected.pop()
+
+def is_possible():
+    points = []
+    for s,e in segments_selected:
+        points.append((s,+1))
+        points.append((e,-1))
+    points.sort(key=lambda x: (x[0],-x[1]))
+
+    count = 0
+    for x,v in points:
+        if count >= 1 and v==1:
+            return False
+    return True
+
+dfs(0)
 print(ans)
