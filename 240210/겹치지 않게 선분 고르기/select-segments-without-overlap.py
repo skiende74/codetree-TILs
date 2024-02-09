e0e7c7973e8058339a924675018cf4a8e7455532
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-10 03:39:22 +0900

    Title: 겹치지 않게 선분 고르기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,18 @@
+N = int(input())
+segments = [list(map(int,input().split())) for _ in range(N)]
+
+points = []
+for s, e in segments:
+    points.append((s,+1))
+    points.append((e,-1))
+
+points.sort()
+
+count = 0
+ans = 0
+for x,v in points:
+    if count == 0 and v==1:
+        ans += 1
+    count += v
+
+print(ans)
