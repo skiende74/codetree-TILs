804a1ced3c0e576b11930fba97e9c0ee7fd783ba
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-10 19:00:29 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -0,0 +1,27 @@
+N = int(input())
+grid = [list(map(int, input().split())) for _ in range(N)]
+grid_dir = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0]*N for _ in range(N)]
+
+
+dir2vec = [0,(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+
+def dfs(i, j):
+    global max_move, move
+
+    d = dir2vec[grid_dir[i][j]]
+    for k in range(1, N):
+        i2, j2 = i+d[0]*k, i+d[1]*k
+        if 0<=i2<N and 0<=j2<N and grid[i2][j2] > grid[i][j]:
+            move += 1
+            max_move = max(max_move, move)
+            dfs(i2, j2)
+            move -= 1
+
+for x in range(N):
+    for y in range(N):
+        move, max_move = 0,0
+        dfs(x, y)
+
+print(max_move)
