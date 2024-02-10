673613a85b3c7b81d2df506a00827cc636336282
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-10 18:43:06 +0900

    Title: 1차원 윷놀이, Time: 1689ms, Memory: 28MB, Status: Passed - CodeTree

--- 240210/1차원 윷놀이/yutnori-1d.py
+++ 240210/1차원 윷놀이/yutnori-1d.py
@@ -9,7 +9,7 @@ def dfs():
         move = [0]*K
         for i, dist in enumerate(distances):
             move[selected[i]] += dist
-        ans = max(ans, sum([1 for m in move if m>=M]))
+        ans = max(ans, sum([1 for m in move if m>=M-1]))
         return
 
     for i in range(K):
