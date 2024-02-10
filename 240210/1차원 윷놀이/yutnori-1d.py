4dbe978d5814c9fb7a2684f3f5a56996b9fa800a
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-10 18:41:49 +0900

    Title: 1차원 윷놀이, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,21 @@
+N, M, K = map(int, input().split())
+distances = list(map(int,input().split()))
+selected = []
+
+ans = 0
+def dfs():
+    global ans
+    if len(selected) == N:
+        move = [0]*K
+        for i, dist in enumerate(distances):
+            move[selected[i]] += dist
+        ans = max(ans, sum([1 for m in move if m>=M]))
+        return
+
+    for i in range(K):
+        selected.append(i)
+        dfs()
+        selected.pop()
+
+dfs()
+print(ans)
