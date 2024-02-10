e7b19924660e68812039a8ecb774255bf59613ad
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-10 19:15:46 +0900

    Title: 순위 경쟁2, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240210/순위 경쟁2/ranking-competition2.py
+++ 240210/순위 경쟁2/ranking-competition2.py
@@ -5,9 +5,27 @@ score= [0, 0]
 max_score = 0
 max_idxs = [1,2]
 
+ans = 0
+state = 2
 for dscore in dscores:
     person, ds = dscore
+    ds = int(ds)
+    
+    state_old = state
+
     if person == 'A':
         score[0] += ds
     else:
         score[1] += ds
+    
+    if score[0] > score[1]:
+        state = 1
+    elif score[0] == score[1]:
+        state = 2
+    else:
+        state = 3
+    if state != state_old:
+        ans+=1
+
+
+print(ans)
