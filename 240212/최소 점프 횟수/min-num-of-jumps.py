a331ffdc796e30fd4f6da5ef16cacdf71190e5d4
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-12 08:30:23 +0900

    Title: 최소 점프 횟수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/최소 점프 횟수/min-num-of-jumps.py
+++ 240212/최소 점프 횟수/min-num-of-jumps.py
@@ -4,5 +4,7 @@ INF = 10**9
 dp = [INF]*(N+1)
 dp[1] = 0
 for i in range(2, N+1):
-    dp[i] = min([INF]+[dp[j]+1 if seq[j]+j >= i else INF for j in range(1, i)])
+    for j in range(1, i):
+        if seq[j]+j >= i:
+            dp[i] = min(dp[i], dp[j]+1)
 print(dp[-1])
