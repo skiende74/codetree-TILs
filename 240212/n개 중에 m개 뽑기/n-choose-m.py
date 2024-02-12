2a8ee9d822359d668cf0060d0dce21c046361b74
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-12 09:00:19 +0900

    Title: n개 중에 m개 뽑기, Time: 222ms, Memory: 25MB, Status: Passed - CodeTree

--- 240212/n개 중에 m개 뽑기/n-choose-m.py
+++ 240212/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,21 +1,19 @@
 N, M = map(int, input().split())
 
-curr_num = []
-used = [False]*(N+1)
-def choose():
-    global curr_num
+combination = []
+def choose(curr_num, cnt):
+    global combination
 
-    if len(curr_num)==M:
-        num = curr_num
-        num = ' '.join(num)
-        print(num)
+    if curr_num == N+1:
+        if cnt==M:
+            print(' '.join(combination))
         return
     
-    for i in range(1,N+1):
-        if not used[i] and (len(curr_num) == 0 or int(curr_num[-1]) < i):
-            used[i]=True
-            curr_num.append(str(i))
-            choose()
-            used[i]=False
-            curr_num = curr_num[:-1]
-choose()
+
+    combination.append(str(curr_num))
+    choose(curr_num + 1, cnt+1)
+    combination = combination[:-1]
+
+    choose(curr_num + 1, cnt)
+
+choose(1,0)
