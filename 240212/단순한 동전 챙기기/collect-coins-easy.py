48d33edc7e07eab76910c6e2aadf4bafc7c1aad1
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-02-12 09:50:59 +0900

    Title: 단순한 동전 챙기기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240212/단순한 동전 챙기기/collect-coins-easy.py
@@ -0,0 +1,39 @@
+N = int(input())
+M = 9
+grid = [list(input()) for _ in range(N)]
+pose = [None] + [None]*9
+for i in range(N):
+    for j in range(N):
+        if grid[i][j] == 'S':
+            start = (i, j)
+        elif grid[i][j] == 'E':
+            end = (i,j)
+        elif grid[i][j] != '.':
+            pose[int(grid[i][j])] = (i,j)
+
+INF = 10**9
+count, ans = 0, INF
+
+def dist(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+def dfs(i, j, chosen):
+    global ans, count
+
+    if i == 9+1:
+        return
+    if j == 3:
+        ans = min(ans, count + dist(chosen, end))
+        return
+    
+    dfs(i+1, j, chosen)
+
+    count += dist(chosen, pose[j+1])
+    dfs(i+1, j+1, pose[j+1])
+    count -= dist(chosen, pose[j+1])
+
+dfs(1, 0, start)
+if ans == INF:
+    print(-1)
+else:
+    print(ans)
