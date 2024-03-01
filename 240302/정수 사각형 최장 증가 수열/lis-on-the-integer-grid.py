64a5e08c6dcf435ed2b70163999096ac00f999aa
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-03-02 05:58:59 +0900

    Title: 정수 사각형 최장 증가 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240302/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240302/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,14 +1,19 @@
+import sys
+sys.setrecursionlimit(10**7)
 N = int(input())
 grid = [list(map(int,input().split())) for _ in range(N)]
 dp = [[1]*N for _ in range(N)]
+
+def dfs(i,j):
+    if dp[i][j] != 1: return dp[i][j]
+    dis, djs = [0,0,-1,1], [-1,1,0,0]
+    for di, dj in zip(dis,djs):
+        i2, j2 = i+di, j+dj
+        if not(0<=i2<N and 0<=j2<N): continue
+        if grid[i][j] > grid[i2][j2]:
+            dp[i][j] = max(dp[i][j], dfs(i2,j2)+1)
+    return dp[i][j]
 for i in range(N):
     for j in range(N):
-        dis, djs = [0,0,-1,1], [-1,1,0,0]
-        for di, dj in zip(dis,djs):
-            i2, j2 = i+di, j+dj
-            if not(0<=i2<N and 0<=j2<N): continue
-            if grid[i][j] > grid[i2][j2]:
-                dp[i][j] = max(dp[i][j], dp[i2][j2]+1)
-            if grid[i][j] < grid[i2][j2]:
-                dp[i2][j2] = max(dp[i2][j2], dp[i][j]+1)
+        dfs(i,j)
 print(max(map(max, *dp)))
