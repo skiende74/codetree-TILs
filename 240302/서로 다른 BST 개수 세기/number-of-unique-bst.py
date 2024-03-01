4f67361d8820102e32d0f56b45ab4ce80bf24ad4
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-03-02 07:22:48 +0900

    Title: 서로 다른 BST 개수 세기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 240302/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -1,18 +1,22 @@
-N = int(input())
-UNUSED=-1
-dp = [1,1]+[UNUSED]*(N-1)
+n = int(input())
 
-def count(i):
-    if dp[i] != UNUSED:
-        return dp[i]
-    
-    if N<= 1: return 1
-    
-    result = 0
-    for i in range(N):
-        result += count(i)*count(N-i-1)
-    dp[i] = result
-    
-    return dp[i]
+UNUSED = -1
 
-print(count(N))
+memo = [UNUSED for _ in range(n + 1)]
+
+def get_num_of_unique_bst(n):
+    if memo[n] != UNUSED:
+        return memo[n]
+    
+    if n <= 1:
+        return 1
+    
+    number_of_unique_bst = 0
+    for i in range(n):
+        number_of_unique_bst += get_num_of_unique_bst(i) * \
+                                get_num_of_unique_bst(n - i - 1)
+        
+    memo[n] = number_of_unique_bst
+    return memo[n]
+
+print(get_num_of_unique_bst(n))
