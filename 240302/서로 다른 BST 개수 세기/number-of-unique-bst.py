e931179799311c4cc585e517545d75d6df8d4366
skiende74 <86130706+skiende74@users.noreply.github.com>
2024-03-02 07:20:47 +0900

    Title: 서로 다른 BST 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240302/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 240302/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -1,5 +1,3 @@
-import sys
-sys.setrecursionlimit(10**5)
 N = int(input())
 UNUSED=-1
 dp = [1,1]+[UNUSED]*(N-1)
@@ -7,6 +5,7 @@ dp = [1,1]+[UNUSED]*(N-1)
 def count(i):
     if dp[i] != UNUSED: return dp[i]
     
+    if n<= 1: return 1
     result = 0
     for i in range(N):
         result += count(i)*count(N-i-1)
