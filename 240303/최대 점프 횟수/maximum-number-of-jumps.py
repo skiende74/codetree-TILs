ce62f35626022ed5dc44872955cb7d3a20594df0
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-03-03 17:59:28 +0900

    Title: 최대 점프 횟수, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240303/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240303/최대 점프 횟수/maximum-number-of-jumps.py
@@ -4,7 +4,7 @@ INT_MIN = -sys.maxsize
 N = int(input())
 seq = list(map(int,input().split()))
 dp = [INT_MIN]*N
-
+dp[0] = 0
 for i in range(1,N):
     for j in range(i):
         if seq[j] >= i-j and dp[j] != INT_MIN:
