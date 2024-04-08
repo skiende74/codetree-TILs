ea9d5115c9f4eb95481e315e3bbe51dd486fa78e
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-09 00:05:35 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240409/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.py
@@ -0,0 +1,22 @@
+from math import ceil
+
+def num2k(num): 
+    result = 0
+
+    for k in range(1,n+1):
+        result += num//k
+    return result
+
+def lower_bound(f, goal):
+    left, right = 1, n*n
+    min_idx = n*n
+
+    while left <= right:
+        mid = (left+right)//2
+        if f(mid) >= goal:
+            right = mid -1
+            min_idx = min(min_idx, mid)
+        else: left = mid + 1
+    return min_idx
+n = int(input())
+print(lower_bound(num2k, int(input())))
