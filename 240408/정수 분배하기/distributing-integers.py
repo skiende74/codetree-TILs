e1e40d0306be66907f27cb81f63be372955f571b
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-08 23:19:29 +0900

    Title: 정수 분배하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240408/정수 분배하기/distributing-integers.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+seq = [int(input()) for _ in range(N)]
+
+def f(k):
+    return sum(map(lambda x: x//k, seq))
+
+def custom_bound(f, goal):
+    left, right = 1, min(seq)
+    max_idx = -1
+
+    while left <= right:
+        mid = (left+right)//2
+
+        if f(mid) >= goal:
+            left = mid + 1
+            max_idx = max(max_idx, mid)
+        else: right = mid-1
+    return max_idx
+print(custom_bound(f, M))
