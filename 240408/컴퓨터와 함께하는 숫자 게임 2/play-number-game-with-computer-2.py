480870ef544470e1d0323133bc81fad3577fcb5a
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-08 18:05:55 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240408/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -0,0 +1,15 @@
+N = int(input())
+def binary(goal):
+    left, right = 1, N
+    
+    count = 0
+    while left<=right:
+        mid = (left+right)//2
+        count += 1
+        if mid == goal: return count 
+        if mid<goal: left = mid+1
+        else: right = mid-1
+
+numbers = list(range(*map(int,input().split())))
+counted = list(map(binary, numbers))
+print(min(counted),max(counted))
