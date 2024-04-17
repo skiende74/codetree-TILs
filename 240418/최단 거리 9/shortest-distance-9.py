0523e5bfa9212ee8fdb0ec8f3cea442847550682
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-18 01:35:26 +0900

    Title: 최단 거리 9, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240418/최단 거리 9/shortest-distance-9.py
@@ -0,0 +1,36 @@
+from heapq import heappop, heappush
+V, E = map(int,input().split())
+
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    i,j,w = map(int,input().split())
+    graph[i].append((j,w))
+    graph[j].append((i,w))
+
+start, end = map(int,input().split())
+path = [-1]*(V+1)
+INF = 10**9
+dist = [INF]*(V+1)
+
+def dijkstra(start):
+    PQ = [(0, start)]
+    dist[start] = 0
+
+    while PQ:
+        d, i = heappop(PQ)
+        if dist[i] < d: continue
+
+        for j,w in graph[i]:
+            if dist[j] <= d+w: continue
+            dist[j] = d+w
+            heappush(PQ, (dist[j], j))
+            path[j] = i
+dijkstra(start)
+
+j = end
+ans = [end]
+while j != start:
+    j = path[j]
+    ans.append(j)
+print(len(ans)-1)
+print(*ans[::-1])
