3d15cb3c2e6c031a88b3d41659d14a0514d93d7a
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-27 05:10:26 +0900

    Title: 크기가 n인 순열, Time: 272ms, Memory: 29MB, Status: Passed - CodeTree

--- 240427/크기가 n인 순열/n-permutation.py
+++ 240427/크기가 n인 순열/n-permutation.py
@@ -1,4 +1,18 @@
-from itertools import permutations
 N = int(input())
-for p in permutations(range(1,N+1),N):
-    print(*p)
+
+def dfs(i):
+    if len(answers)==N:
+        print(*answers)
+    for j in range(1,N+1):
+        if not visited[j]:
+            visited[j] = True
+            answers.append(j)
+            dfs(j)
+            answers.pop()
+            visited[j] = False
+
+for i in range(1,N+1):
+    visited = [False]*(N+1)
+    visited[i] = True
+    answers = [i]
+    dfs(i)
