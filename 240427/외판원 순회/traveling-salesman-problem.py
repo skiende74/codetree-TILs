e7df149479d3e9816e83901ba8c830e219ff35e2
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-27 05:30:03 +0900

    Title: 외판원 순회, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240427/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,23 @@
+N = int(input())
+grid = [[0]*(N+1)]+[[0]+list(map(int,input().split())) for _ in range(N)]
+
+answer = 0
+max_val = 10**9
+def dfs(i):
+    global max_val, answer, cnt
+    if cnt == N:
+        if i==1: max_val = min(max_val, answer)
+        return
+    for j in range(1,N+1):
+        if visited[j]: continue
+        visited[j] = True
+        answer += grid[i][j]
+        cnt += 1
+        dfs(j)
+        cnt -= 1
+        answer -= grid[i][j]
+        visited[j] = False
+cnt = 0
+visited = [False]*(N+1)
+dfs(1)
+print(max_val)
