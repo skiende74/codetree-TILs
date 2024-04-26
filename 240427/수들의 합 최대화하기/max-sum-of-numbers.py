83ca746833f341b27f1bed4df1fc6d2981c9b8e7
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-27 05:20:14 +0900

    Title: 수들의 합 최대화하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240427/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(2*10**5)
+
+N = int(input())
+grid = [list(map(int,input().split())) for _ in range(N)]
+visited = set()
+
+max_val = -1
+def dfs(i):
+    global max_val
+    if len(answers)==N:
+        max_val = max(max_val, sum(answers))
+        return
+    for j in range(N):
+        if j in visited: continue
+        visited.add(j)
+        answers.append(grid[i][j])
+        dfs(j)
+        answers.pop()
+        visited.remove(j)
+
+answers= []
+dfs(0)
+print(max_val)
