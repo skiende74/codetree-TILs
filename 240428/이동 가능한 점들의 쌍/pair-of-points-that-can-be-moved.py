08ebb8e20e1622cf27e8b8ec25c6ce3076755cc9
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-28 04:15:14 +0900

    Title: 이동 가능한 점들의 쌍, Time: 406ms, Memory: 26MB, Status: Passed - CodeTree

--- 240428/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
+++ 240428/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -2,27 +2,28 @@ V, E, P, Q = map(int,input().split())
 
 INF = 10**9
 grid=[[INF]*(V+1) for _ in range(V+1)]
-red_pass=[[False]*(V+1) for _ in range(V+1)]
 for _ in range(E):
     i,j,w = map(int,input().split())
     grid[i][j] = w
 
+for i in range(1, V+1):
+    grid[i][i] = 0
 for k in range(1,V+1):
     for i in range(1,V+1):  
         for j in range(1,V+1):
-            if grid[i][k] + grid[k][j] < grid[i][j]:
-                grid[i][j] = grid[i][k] + grid[k][j]
-                if i<=P or j<=P or k<=P:
-                    red_pass[i][j] = True
-
-def is_red(i,j):
-    return red_pass[i][j] or i<=P or j<=P
-sum_ = 0
+            grid[i][j] = min(grid[i][j], grid[i][k] + grid[k][j])
+ans = 0
 count_ = 0
 for _ in range(Q):
     i, j = map(int,input().split())
-    sum_ += grid[i][j] if is_red(i,j) else 0
-    count_ += 1 if is_red(i,j) else 0
+    
+    dist = INF
+    for a in range(1,P+1):
+        dist = min(dist, grid[i][a]+grid[a][j])
+    if dist >= INF: continue
 
+    ans += dist
+    count_ += 1
+        
 print(count_)
-print(sum_)
+print(ans)
