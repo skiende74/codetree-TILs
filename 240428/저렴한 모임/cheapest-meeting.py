f3cddfe16e1ecf103dc673ff241580f90fa361ed
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-04-28 05:01:24 +0900

    Title: 저렴한 모임, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240428/저렴한 모임/cheapest-meeting.py
@@ -0,0 +1,21 @@
+V, E = map(int,input().split())
+s1,s2, e = map(int,input().split())
+
+INF = 10**9
+grid = [[INF]*(V+1) for _ in range(V+1)]
+for i in range(1,V+1): grid[i][i]=0
+for _ in range(E):
+    i,j,w = map(int,input().split())
+    grid[i][j] = w
+    grid[j][i] = w
+
+for k in range(1,V+1):
+    for i in range(1,V+1):
+        for j in range(1,V+1):
+            grid[i][j] = min(grid[i][j], grid[i][k]+grid[k][j])
+
+#print(*grid,sep='\n')
+ans = INF
+for i in range(1,V+1):
+    ans=min(ans,grid[s1][i]+grid[s2][i]+grid[i][e])
+print(ans)
