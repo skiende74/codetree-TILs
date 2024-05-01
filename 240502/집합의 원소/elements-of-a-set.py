cb63b246c81c02e3b64a09640a5bc8234ba0fdfa
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-05-02 04:51:02 +0900

    Title: 집합의 원소, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240502/집합의 원소/elements-of-a-set.py
@@ -0,0 +1,18 @@
+def union(i,j):
+    r1,r2 = find(i), find(j)
+    r = min(r1,r2)
+    parent[r1] = r
+    parent[r2] = r
+def find(i):
+    if parent[i] == i: return i
+    parent[i] = find(i)
+    return parent[i]
+
+N, M = map(int,input().split())
+parent = [i for i in range(N+1)]
+for _ in range(M):
+    op, i, j = map(int,input().split())
+    if op == 0:
+        union(i,j)
+    else:
+        print(1 if find(i) == find(j) else 0 )
