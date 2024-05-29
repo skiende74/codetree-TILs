1d5b094977f81cd25eae95fac53562a14b4d1efa
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-05-30 04:36:58 +0900

    Title: 중복되는 수열, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240530/중복되는 수열/duplicate-sequence.py
@@ -0,0 +1,23 @@
+N = int(input())
+words = [input() for _ in range(N)]
+
+cnt = 0
+class TrieNode():
+    global cnt
+    def __init__(self):
+        self.is_end = False
+        self.children = [None]*10
+    def insert_word(self, s):
+        t = self
+        for char in s:
+            index = int(char)
+            if t.children[index] is None:
+                t.children[index] = TrieNode()
+            t = t.children[index]
+        if t.is_end: cnt += 1
+        t.is_end = True
+root = TrieNode()
+
+for word in words:
+    root.insert_word(word)
+print(cnt)
