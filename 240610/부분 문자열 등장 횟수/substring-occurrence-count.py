e8eff047cbb9c1ce201dd5acbb20cbb429138354
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-06-10 16:31:20 +0900

    Title: 부분 문자열 등장 횟수, Time: 174ms, Memory: 26MB, Status: Passed - CodeTree

--- 240610/부분 문자열 등장 횟수/substring-occurrence-count.py
+++ 240610/부분 문자열 등장 횟수/substring-occurrence-count.py
@@ -10,15 +10,15 @@ P = '#'+P
 
 f = [0]*(M+1)
 f[0] = -1
-for i in range(1, M+1):
-    j = f[i-1]
-    while j>=0 and P[j+1] != P[i]: j = f[j]
-    f[i] = j + 1
+j = -1
+for i in range(1,M+1):
+    while j>=0 and P[i] != P[j+1]: j = f[j]
+    j += 1
+    f[i] = j
 
 cnt = 0
-j = 0
 for i in range(1, N+1):
-    while j>=0 and P[j+1] != T[i]: j = f[j]
+    while j>=0 and T[i] != P[j+1]: j = f[j]
     j += 1
     if j == M:
         cnt += 1
