9c2fce73fd7192c5f132f6930998e994ccc8bedd
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-07 08:47:46 +0900

    Title: bit 계산, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240707/bit 계산/bit-calculation.py
+++ 240707/bit 계산/bit-calculation.py
@@ -1,8 +1,9 @@
 N = int(input())
 num = 0
 for _ in range(N):
-    op, n = input().split()
-    n = int(n)
+    op, *n = input().split()
+    
+    if n: n = int(n[0])
     
     if op == 'add' and num & 1 << n == 0:
         num ^= 1 << n
