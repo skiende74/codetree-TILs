a61dab895b31f4469d89dbd4698323fa88816067
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-07 08:46:54 +0900

    Title: bit 계산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240707/bit 계산/bit-calculation.py
@@ -0,0 +1,16 @@
+N = int(input())
+num = 0
+for _ in range(N):
+    op, n = input().split()
+    n = int(n)
+    
+    if op == 'add' and num & 1 << n == 0:
+        num ^= 1 << n
+    elif op == 'delete' and num & 1 << n == 1:
+        num ^= 1 << n
+    elif op == 'print':
+        print( 1 if num & 1 << n else 0)
+    elif op == 'toggle':
+        num ^= 1 << n
+    else:
+        num = 0
