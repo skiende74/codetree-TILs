fc4bd6e65cf16f2fc3a8299467f4c628692cda2d
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-07 08:59:59 +0900

    Title: bit 계산, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240707/bit 계산/bit-calculation.py
+++ 240707/bit 계산/bit-calculation.py
@@ -8,7 +8,7 @@ for _ in range(N):
     if op == 'add':
         num |= 1 << n
     elif op == 'delete' and num>>n & 1 == 1:
-        num ^= (1 << n)
+        num &= ~(1 << n)
     elif op == 'print':
         print( 1 if num & (1 << n) else 0)
     elif op == 'toggle':
