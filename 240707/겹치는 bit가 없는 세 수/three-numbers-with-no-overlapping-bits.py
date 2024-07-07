30a11aacb2d45b97cee28b9ce24a15667338b4f7
Seyeong Jeon <86130706+skiende74@users.noreply.github.com>
2024-07-07 09:14:22 +0900

    Title: 겹치는 bit가 없는 세 수, Time: 382ms, Memory: 26MB, Status: Passed - CodeTree

--- 240707/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.py
+++ 240707/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.py
@@ -4,6 +4,6 @@ ans = 0
 for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
-            if seq[i] & seq[j] & seq[k] == 0:
+            if not(seq[i] & seq[j] or seq[j] & seq[k] or seq[k] & seq[i]):
                 ans = max(ans, seq[i]|seq[j]|seq[k])
 print(ans)
